c43a3fb0680588c1d520bdcc9f4d4d99c0ef572f
Charlie <charlieabel99@gmail.com>
2025-11-10 14:02:44 +0000

    added calculator function for how long I've been learning to code

--- assets/scripts/website_scripts/myHomePage.js
+++ assets/scripts/website_scripts/myHomePage.js
@@ -2,3 +2,13 @@ const tellFortune = () => {
     let display = document.getElementById('fortune');
     display.innerHTML = generateMessage();
 }
+
+const codingSince = () => {
+    let started = new Date('2025-06-16');
+    let now = new Date;
+
+    timeCoding = Math.floor((now - started) / 1000 / 60 / 60 /24);
+    document.getElementById('time-coding').innerHTML = timeCoding + ' days';
+}
+
+codingSince();
