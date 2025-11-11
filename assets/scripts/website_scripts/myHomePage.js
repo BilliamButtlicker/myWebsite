0b2458930120d3c6113b50343d966f2d0b625162
Charlie <charlieabel99@gmail.com>
2025-11-11 12:38:19 +0000

    added a slider function  for displaying hobbies, and fleshed out index content a bit. added pages for 'piano' and 'languages'.

--- assets/scripts/website_scripts/myHomePage.js
+++ assets/scripts/website_scripts/myHomePage.js
@@ -17,3 +17,38 @@ const codingSince = () => {
 }
 
 codingSince();
+
+const slider = () => {
+    let hobImgs = ['','']
+    let hobAlts = ['Photo of my playing the piano','Languages photo']
+    let hobbies = ['Piano','Languages'];
+    let description = [
+        'I\'ve always loved to play the piano - one of my favourite pastimes is sitting down and working out the notes and chords of a new sog I\'ve discovered!',
+        'Languages play a huge part in all of our lives. I grew up speaking English and Swedish, but languages are not only verbal...'
+    ];
+    let link = ['hobbies/piano.html','hobbies/languages.html'];
+
+    let n = document.getElementById('sliderIndex').value;
+
+    n > hobbies.length - 1 ? n = 0 : n = n;
+
+
+    let hobbyImg = document.getElementById('hob-img');
+    let hobby = document.getElementById('hob-title');
+    let hobDesc = document.getElementById('hob-desc');
+    let hobLink = document.getElementById('hob-link');
+
+
+    hobbyImg.setAttribute('src',hobImgs[n]);
+    hobbyImg.setAttribute('alt',hobAlts[n]);
+
+    hobLink.setAttribute('href',link[n]);
+
+    hobby.innerHTML = hobbies[n];
+    hobDesc.innerHTML = description[n];
+
+    n++;
+    document.getElementById('sliderIndex').value = n;
+}
+slider();
+setInterval(function() {slider()}, 5000);
